0343e381d5e827a05a6cd253f5163090890c9930
PieterDr <pieter.drijkoningen@vrt.be>
2020-11-08 13:08:15 +0100

    Setup project

---
+++ src/main/java/com/wackygiraffe/immoaggregator/ImmoAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.wackygiraffe.immoaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wackygiraffe/immoaggregator/ImmoAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wackygiraffe.immoaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
